d5a5bc3f7ec4aab16a0e99d0daac04010ce1dce5
Hasan Can Büyükaşık <hasancan.buyukasik@fibabanka.com.tr>
2021-02-10 21:11:36 +0300

    Project Init commit

---
+++ app/src/main/java/com/obss/rickandmorty/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.obss.rickandmorty
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.obss.rickandmorty.base.BaseActivity
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.support.HasSupportFragmentInjector
+import javax.inject.Inject
+
+class MainActivity : BaseActivity(), HasSupportFragmentInjector {
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun supportFragmentInjector() = dispatchingAndroidInjector
+}
