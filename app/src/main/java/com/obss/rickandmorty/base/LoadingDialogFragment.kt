7e6c3e9a60a8532bafc9681c7754575789d9eafa
Hasan Can Büyükaşık <hasancan.buyukasik@fibabanka.com.tr>
2021-02-10 21:19:27 +0300

    Loading Dialog fragment added into the project

---
+++ app/src/main/java/com/obss/rickandmorty/base/LoadingDialogFragment.kt
@@ -0,0 +1,32 @@
+package com.obss.rickandmorty.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
+import com.obss.rickandmorty.R
+import com.obss.rickandmorty.databinding.LoadingDialogFragmentBinding
+
+
+class LoadingDialogFragment : DialogFragment() {
+
+    lateinit var binding: LoadingDialogFragmentBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NO_TITLE, R.style.LoadingDialog)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.loading_dialog_fragment, container, false)
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/obss/rickandmorty/model/ErrorPopUpMessage.kt
@@ -0,0 +1,6 @@
+package com.obss.rickandmorty.model
+
+class ErrorPopUpMessage(
+    val title: String,
+    val message : String
+)
