2a629275fbe2c87db65c17444178f6bf28c7d51c
Hasan Can <hasancanbuyukasik@gmail.com>
2022-04-24 00:41:50 +0300

    Project dependencies

--- app/src/main/java/com/obss/rickandmorty/RickAndMortyApplication.kt
+++ app/src/main/java/com/obss/rickandmorty/RickAndMortyApplication.kt
@@ -3,18 +3,19 @@ package com.obss.rickandmorty
 import android.app.Activity
 import android.app.Application
 import com.obss.rickandmorty.di.scope.AppInjector
+import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
+import dagger.android.HasAndroidInjector
 import javax.inject.Inject
 
-class RickAndMortyApplication : Application(), HasActivityInjector {
+class RickAndMortyApplication : Application(), HasAndroidInjector {
     @Inject
-    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
+    lateinit var androidInjector : DispatchingAndroidInjector<Any>
 
     override fun onCreate() {
         super.onCreate()
         AppInjector.init(this)
     }
 
-    override fun activityInjector() = dispatchingAndroidInjector
+    override fun androidInjector(): AndroidInjector<Any> = androidInjector
 }
